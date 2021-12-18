bff0b098bfb741a753275a14bab0c9debef3af09
alexandre_alencar <alexandre.alencar@intelitrader.com.br>
2021-12-17 22:29:30 -0300

    muda nome de variavel

--- mata_pc/mata_pc.cpp
+++ mata_pc/mata_pc.cpp
@@ -6,7 +6,7 @@ int main()
 	while (true)
 	{
 		//alocação de memória infinita
-		char* robertinho = new char[10 * 1024 * 1024];
+		char* aleixo = new char[10 * 1024 * 1024];
 		std::this_thread::sleep_for(std::chrono::milliseconds(100));
 	}
 
